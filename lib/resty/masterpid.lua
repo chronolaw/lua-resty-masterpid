262ec7fd0c500f8555538a2ede027acc8d8cd651
chronolaw <chrono_cpp@me.com>
2017-12-26 13:25:49 +0800

    masterpid.lua

---
+++ lib/resty/masterpid.lua
@@ -0,0 +1,32 @@
+-- Copyright (C) 2017 by chrono
+
+local ffi = require "ffi"
+local ffi_cdef = ffi.cdef
+local ffi_C = ffi.C
+
+local ngx_worker_pid = ngx.worker.pid
+
+ffi_cdef[[
+    typedef int pid_t;
+    pid_t getppid(void);
+]]
+
+ffi_cdef[[
+    typedef uintptr_t ngx_uint_t;
+    extern ngx_uint_t ngx_process;
+]]
+
+
+local function get_masterpid()
+    if ffi.os ~= "Linux" then
+        return nil, "only work in linux"
+    end
+
+    -- 0 = single process
+    local ppid = (ffi_C.ngx_process == 0) and
+                    ngx_worker_pid() or ffi_C.getppid()
+
+    return ppid
+end
+
+return get_masterpid
